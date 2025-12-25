c38eff26514a407ddc57e50d49312d142464f79c
João Pedro Olavo De Matos <pjoao3672@gmail.com>
2025-12-25 14:16:05 -0300

    feat: introduce src structure and centralize feature definitions

---
+++ src/features.py
@@ -0,0 +1,36 @@
+NUMERIC_FEATURES = [
+    "LotArea",
+    "GrLivArea",
+    "TotalBsmtSF",
+    "1stFlrSF",
+    "2ndFlrSF",
+    "GarageArea",
+    "WoodDeckSF",
+    "OpenPorchSF"
+]
+
+ORDINAL_FEATURES = [
+    "OverallQual",
+    "OverallCond",
+    "ExterQual",
+    "ExterCond",
+    "KitchenQual",
+    "HeatingQC",
+    "GarageQual",
+    "GarageCond"
+]
+
+NOMINAL_FEATURES = [
+    "Neighborhood",
+    "MSZoning",
+    "BldgType",
+    "HouseStyle",
+    "RoofStyle",
+    "Exterior1st"
+]
+
+TEMPORAL_FEATURES = [
+    "YearBuilt",
+    "YearRemodAdd",
+    "YrSold"
+]
